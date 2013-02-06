3332e15e2dc490f0f98c9493d46e15c4273315b5
Chirstopher Ottley <dev@referencelogic>
2013-02-06 11:33:56 -0400

    Bumped up version

--- jquery_load_json/controller.php
+++ jquery_load_json/controller.php
@@ -6,7 +6,7 @@ class jqueryloadjsonPackage extends Package {
 
 	protected $pkgHandle = 'jquery_load_json';
 	protected $appVersionRequired = '5.3.3'; 
-	protected $pkgVersion = '1.0';
+	protected $pkgVersion = '1.0.1';
 	
 	public function getPackageDescription() {
 		return t("Lets you add a block where you can populate an html template using JSON.");
